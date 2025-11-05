a52ee8673e6fcb0c7aa7b74067910b7bc4eaa129
Mathieu LESNIAK <mathieu@lesniak.fr>
2025-11-05 08:49:12 +0100

    input type time

---
+++ src/Form/Time.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Suricate\Bootstrap5\Form;
+
+use Suricate\FormItem;
+
+/**
+ * Render a bootstrap input time
+ *
+ * @author Mathieu LESNIAK <mathieu@lesniak.fr>
+ */
+class Time
+{
+    /**
+     * Bootstrap input time
+     *
+     * @param string $name           input name
+     * @param string $value          input value
+     * @param array  $htmlAttributes form item params
+     *
+     * @return string
+     */
+    public static function render(string $name, $value, array $htmlAttributes = []): string
+    {
+        $label = isset($htmlAttributes['label']) ? $htmlAttributes['label'] : '';
+        if (isset($htmlAttributes['class'])) {
+            $htmlAttributes['class'] = 'form-control ' . $htmlAttributes['class'];
+        } else {
+            $htmlAttributes['class'] = 'form-control';
+        }
+
+        $output = '<div class="mb-3">';
+        $output .= FormItem::time($name, $value, $label, $htmlAttributes);
+        $output .= '</div>';
+
+        return $output;
+    }
+}
